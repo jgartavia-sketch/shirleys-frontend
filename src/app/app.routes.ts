9c8f848ffd6cb04d434d2f8f304a7bc4c95ba38f
TU_NOMBRE <jgartavia@gmail.com>
2026-05-08 20:13:16 -0600

    Initial Shirley's Angular restaurant frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'inicio',
+    loadComponent: () =>
+      import('./pages/inicio/inicio').then(m => m.Inicio)
+  },
+
+  {
+    path: 'menu',
+    loadComponent: () =>
+      import('./pages/menu/menu').then(m => m.Menu)
+  },
+
+  {
+    path: 'catering',
+    loadComponent: () =>
+      import('./pages/catering/catering').then(m => m.Catering)
+  },
+
+  {
+    path: 'nosotros',
+    loadComponent: () =>
+      import('./pages/nosotros/nosotros').then(m => m.Nosotros)
+  },
+
+  {
+    path: 'contacto',
+    loadComponent: () =>
+      import('./pages/contacto/contacto').then(m => m.Contacto)
+  }
+];
---
+++ src/app/pages/catering/catering.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-catering',
+  imports: [RouterLink],
+  templateUrl: './catering.html',
+  styleUrl: './catering.css',
+})
+export class Catering {}
