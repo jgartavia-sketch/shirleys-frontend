000544f7f7f34a54189368dc3d4ae5612d32abfe
TU_NOMBRE <jgartavia@gmail.com>
2026-05-15 12:24:58 -0600

    Connect admin login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -56,6 +56,12 @@ export const routes: Routes = [
   },
 
   {
+    path: 'admin-login',
+    loadComponent: () =>
+      import('./pages/admin-login/admin-login').then(m => m.AdminLogin)
+  },
+
+  {
     path: 'admin',
     loadComponent: () =>
       import('./pages/admin/admin').then(m => m.Admin)
