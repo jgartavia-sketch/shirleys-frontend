c10f95db206dd4afde32670e6c3c07381ae174f5
TU_NOMBRE <jgartavia@gmail.com>
2026-05-17 18:05:36 -0600

    feat: initialize Android app with Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shirleyscr.app',
+  appName: 'Shirleys CR',
+  webDir: 'dist/Shirleys/browser'
+};
+
+export default config;
